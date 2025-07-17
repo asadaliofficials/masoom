dec86efb3eef681a13da40ca24160df94868419d
Asad Ali <asadjattofficials@gmail.com>
2025-07-17 22:25:00 +0500

    created folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => {
-	return <div className="text-ivory font-black uppercase underline tracking-widest">App</div>;
+	return <div className="text-dustyRose font-black uppercase underline tracking-widest">App</div>;
 };
 
 export default App;
