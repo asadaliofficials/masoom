9148dbd7d13ec20465566e8dedb1a399fdc8d21e
Asad Ali <asadjattofficials@gmail.com>
2025-07-18 06:03:50 +0500

    top bar responsived

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import TopBar from './components/TopBar';
+import { BrowserRouter } from 'react-router-dom';
 
 const App = () => {
 	return (
 		<>
-			<div className="app">
-				<TopBar />
-			</div>
+			<BrowserRouter>
+				<div className="app max-w-[1440px]">
+					<TopBar />
+				</div>
+			</BrowserRouter>
 		</>
 	);
 };
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,38 +1,60 @@
-import React from 'react';
-
+import { NavLink } from 'react-router-dom';
+import '../styles/topBar.css';
+import { Menu } from 'lucide-react';
 const TopBar = () => {
+	// Tailwind class common for all links
+	const baseClass =
+		'w-max opacity-40 hover:opacity-100 transition-all duration-300 cursor-pointer font-semibold';
+
 	return (
-		<>
-			<div className="py-1 px-8 border-b-2 border-[#F2F2F2] flex justify-between">
-				<p className="text-muted">#1 Brand of Fashion in ASIA</p>
-				<div className="flex gap-4 select-none">
-					<ul className="flex gap-4 ">
-						<li className="opacity-60 hover:opacity-100 transition-all duration-300 cursor-pointer">
-							Home
-						</li>
-						<li className="opacity-60 hover:opacity-100 transition-all duration-300 cursor-pointer">
-							Products
-						</li>
-						<li className="opacity-60 hover:opacity-100 transition-all duration-300 cursor-pointer">
-							Meet Masoom
-						</li>
-						<li className="opacity-60 hover:opacity-100 transition-all duration-300 cursor-pointer">
-							Masoom's Success
-						</li>
-						<li className="opacity-60 hover:opacity-100 transition-all duration-300 cursor-pointer">
-							Press
-						</li>
-						<li className="opacity-60 hover:opacity-100 transition-all duration-300 cursor-pointer">
-							About us
-						</li>
-						|
-					</ul>
-					<div className="font-bold opacity-80 hover:opacity-100 transition-all duration-300 cursor-pointer">
+		<div className="topBar  w-[100%] py-1.5 px-8 border-b-2 border-[#F2F2F2] flex justify-between">
+			<p className="noOneBrand text-muted">#1 Fashion Brand in ASIA</p>
+			<div className="links flex gap-16 select-none">
+				<Menu className="hidden menuIcon" />
+				<p className="noOneBrandCenter hidden text-muted">#1 Fashion Brand in ASIA</p>
+				<ul className="flex gap-4 ">
+					<NavLink
+						to="/"
+						end
+						className={({ isActive }) => `${baseClass} ${isActive ? 'font-bold opacity-100' : ''}`}
+					>
+						Home
+					</NavLink>
+					<NavLink
+						to="/products"
+						className={({ isActive }) => `${baseClass} ${isActive ? 'font-bold opacity-100' : ''}`}
+					>
+						Products
+					</NavLink>
+					<NavLink
+						to="/meet-masoom"
+						className={({ isActive }) => `${baseClass} ${isActive ? 'font-bold opacity-100' : ''}`}
+					>
+						Meet Masoom
+					</NavLink>
+					<NavLink
+						to="/masoom-success-story"
+						className={({ isActive }) => `${baseClass} ${isActive ? 'font-bold opacity-100' : ''}`}
+					>
+						Masoom's Success
+					</NavLink>
+					<NavLink
+						to="/about"
+						className={({ isActive }) => `${baseClass} ${isActive ? 'font-bold opacity-100' : ''}`}
+					>
+						About us
+					</NavLink>
+				</ul>
+				<div className="font-bold opacity-80 hover:opacity-100 transition-all duration-300 cursor-pointer">
+					<NavLink
+						to="/login"
+						className={({ isActive }) => (isActive ? 'font-bold opacity-100' : '')}
+					>
 						Login
-					</div>
+					</NavLink>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 };
 
