5beeabaf3949a213ec4bc639483dd0d934ca3763
Asad Ali <asadjattofficials@gmail.com>
2025-07-21 13:38:10 +0500

    best seller responsived

--- src/components/home/BestSellerCard.jsx
+++ src/components/home/BestSellerCard.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-
+import '../../styles/home/bestSeller.css';
 const BestSellerCard = () => {
 	return (
-		<div className="h-[480px] mx-auto w-full max-w-[370px] rounded-lg  shoadow-xl flex flex-col items-center  border-b border-gray-200 bg-white cursor-pointer">
+		<div className="bestSellerCard h-[480px] mx-auto w-full max-w-[370px] rounded-lg  shoadow-xl flex flex-col items-center  border-b border-gray-200 bg-white cursor-pointer">
 			<div className="h-[89%]  w-full overflow-hidden rounded-t-lg">
 				<img
-          className="w-full h-full object-contain"
+					className="w-full h-full object-cover"
 					src="https://d2bl4mvd8nzejc.cloudfront.net/327-large_minimal_fashion/linen-blend-tuxedo-jacket.jpg"
 					alt="img"
 				/>
 			</div>
-      <div className='h-[11%] w-full px-2 py-1 font-semibold leading-tight hover:text-blue-600 transition-all duration-300'>
+			<div className="h-[11%] w-full px-2 py-1 font-semibold leading-tight hover:opacity-100 transition-all duration-300 opacity-70">
 				<h2>The Title of this dress, and what if this has long title </h2>
 			</div>
 		</div>
--- src/layouts/home/BestSellerHome.jsx
+++ src/layouts/home/BestSellerHome.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import BestSellerCard from '../../components/home/BestSellerCard';
+import '../../styles/home/bestSeller.css';
 
 const BestSellerHome = () => {
 	return (
-		<div className='p-20 bg-gray-100 '>
-			<h2 className='text-4xl font-bold tracking-wide'>Best Seller Products</h2>
-			<div className='flex gap-12 mt-8'>
+		<div className='bestSeller p-20 bg-gray-100 '>
+			<h2 className='heading text-4xl font-bold tracking-wide'>Best Seller Products</h2>
+			<div className='bestSellerCards flex gap-12 mt-8 justify-center'>
 				<BestSellerCard />
 				<BestSellerCard />
 				<BestSellerCard />
