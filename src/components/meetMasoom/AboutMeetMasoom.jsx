401903944d695a211ac3e5b03434b3fb79a744ec
Asad Ali <asadjattofficials@gmail.com>
2025-07-25 12:09:31 +0500

    path resolved

--- src/components/meetMasoom/AboutMeetMasoom.jsx
+++ src/components/meetMasoom/AboutMeetMasoom.jsx
@@ -4,7 +4,7 @@ import ScrollFloat from '../react-bits/ScrollFloat/ScrollFloat';
 import { motion, useMotionValue, useTransform } from 'framer-motion';
 import { useRef } from 'react';
 import '../../styles/home/aboutHome.css';
-import { AnimatedTooltip } from '../aceternityUI/AnimaedToolTip.jsx';
+import { AnimatedTooltip } from '../aceternityUI/AnimatedToolTip.jsx';
 
 const AboutHome = () => {
 	const imgRef = useRef(null);
